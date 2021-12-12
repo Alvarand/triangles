5ed7b928a12780ff01fc307d35dd7a56321a7b47
Alvarand <dgalil.1999@mail.ru>
2021-12-12 21:51:27 +0300

    added rendering recent lines

--- render.py
+++ render.py
@@ -9,20 +9,23 @@ class Render:
         self.screen = pygame.display.set_mode(WINDOW)
         self.screen.fill(WHITE)
         self.lines = []
-        self.x_y_start = (0, 0)
+        self.start_pos = (0, 0)
 
-    def update_start(self, x, y):
-        self.x_y_start = x, y
+    def update_start(self, pos):
+        self.start_pos = pos
 
     def render_lines(self):
         for line in self.lines:
-            pygame.draw(self.screen, BLUE, (line[0], line[1]), (line[2], line[3]))
+            pygame.draw.line(self.screen, BLUE, (line[0], line[1]), (line[2], line[3]))
 
-    def create_line(self, x, y):
-        pygame.draw.line(self.screen, BLUE, self.x_y_start, (x, y))
+    def create_line(self, end_pos):
+        pygame.draw.line(self.screen, BLUE, self.start_pos, end_pos)
 
     def reload_screen(self):
         self.screen.fill(WHITE)
+
+    def append_lines(self, end_pos):
+        self.lines.append([self.start_pos[0], self.start_pos[1], end_pos[0], end_pos[1]])
 
 
 window = Render()
@@ -40,18 +43,17 @@ while running:
             if click[0] is True:
                 if first is True:
                     first, second = second, first
-                    window.update_start(position[0], position[1])
+                    window.update_start(position)
                 elif second is True:
                     first, second = second, first
                     window.reload_screen()
-                    window.create_line(position[0], position[1])
-
+                    window.append_lines(position)
             if click[2] is True:
+                window.lines = []
                 window.reload_screen()
     if second is True:
         window.reload_screen()
-        position = pygame.mouse.get_pos()
-        window.create_line(position[0], position[1])
+        window.create_line(pygame.mouse.get_pos())
     window.render_lines()
     pygame.display.update()
 
