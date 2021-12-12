c2097042c4647f32dad2fcd2f15a3143111ac05c
Alvarand <dgalil.1999@mail.ru>
2021-12-12 21:56:38 +0300

    code was changed to class

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import pygame
-from func import reload
-from config import *
+from config import running, first, second
+from render import Render
 
-
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(WHITE)
-pygame.display.set_caption('Типа игра')
-clock = pygame.time.Clock()
-
+window = Render()
 
 while running:
-    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -20,24 +13,22 @@ while running:
                 running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             click = pygame.mouse.get_pressed()
+            position = pygame.mouse.get_pos()
             if click[0] is True:
                 if first is True:
-                    x_start, y_start = pygame.mouse.get_pos()
                     first, second = second, first
+                    window.update_start(position)
                 elif second is True:
-                    x_end, y_end = pygame.mouse.get_pos()
-                    reload(screen)
-                    pygame.draw.line(screen, BLUE, (x_start, y_start), (x_end, y_end))
-                    lines.append([x_start, y_start, x_end, y_end])
                     first, second = second, first
+                    window.reload_screen()
+                    window.append_lines(position)
             if click[2] is True:
-                lines = []
-                reload(screen)
+                window.lines = []
+                window.reload_screen()
     if second is True:
-        reload(screen)
-        pygame.draw.line(screen, BLUE, (x_start, y_start), pygame.mouse.get_pos())
-    for line in lines:
-        pygame.draw.line(screen, BLUE, (line[0], line[1]), (line[2], line[3]))
+        window.reload_screen()
+        window.create_line(pygame.mouse.get_pos())
+    window.render_lines()
     pygame.display.update()
 
 pygame.quit()
