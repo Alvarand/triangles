f2fb1df6468c3a1fd8999ac6f730fbb2b842af26
Alvarand <dgalil.1999@mail.ru>
2021-12-12 22:55:25 +0300

    added different colors

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
+import random
+
+
+def random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
 running = True
-WINDOW = WIDTH, HEIGHT = 1024, 512
+WINDOW = WIDTH, HEIGHT = 600, 512
 FPS = 60
+GREY = (220, 220, 220)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -8,3 +16,10 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 first, second = True, False
 lines = []
+rects = [[0, 0, 100, 100, RED],
+         [100, 0, 100, 100, BLUE],
+         [200, 0, 100, 100, GREEN],
+         [300, 0, 100, 100, random_color()],
+         [400, 0, 100, 100, random_color()],
+         [500, 0, 100, 100, random_color()],
+         ]
