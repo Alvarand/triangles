d28a781fa5ffad8cedcb4afb6a59e21d2c6112be
Alvarand <dgalil.1999@mail.ru>
2021-12-13 21:14:16 +0300

    added creating triangle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from config import running, first, second, FPS
+from config import running, angle, FPS
 from render import Render
 
 window = Render()
@@ -16,19 +16,24 @@ while running:
             position = pygame.mouse.get_pos()
             if position[1] >= 100:
                 if click[0] is True:
-                    if first is True:
-                        first, second = second, first
-                        window.update_start(position)
-                    elif second is True:
-                        first, second = second, first
+                    if angle == 0:
+                        angle += 1
+                        window.update_first_angle(position)
+                    elif angle == 1:
+                        angle += 1
                         window.reload_screen()
                         window.append_lines(position)
+                        window.update_start(position)
+                    elif angle == 2:
+                        angle = 0
+                        window.reload_screen()
+                        window.create_triangle(position)
             else:
                 window.get_color(position)
             if click[2] is True:
                 window.update_lines()
                 window.reload_screen()
-    if second is True:
+    if angle != 0:
         window.reload_screen()
         window.create_line(pygame.mouse.get_pos())
     window.render_lines()
